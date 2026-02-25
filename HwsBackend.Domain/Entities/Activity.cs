7aa95de4c14387f4200a238c12f649d6a3a560f4
adelekekismath <adelekekismath9@gmail.com>
2026-02-25 11:55:36 +0100

    Add Activity and Guide entity classes with properties

---
+++ HwsBackend.Domain/Entities/Activity.cs
@@ -0,0 +1,17 @@
+
+public class Activity {
+    public int Id { get; set; }
+    public string Title { get; set; } 
+    public string Description { get; set; } 
+    public string Category { get; set; } 
+    public string Address { get; set; } 
+    public string PhoneNumber { get; set; } 
+    public string OpeningHours { get; set; } 
+    public string Website { get; set; } 
+
+
+    public int DayNumber { get; set; } 
+    public int ExecutionOrder { get; set; } 
+
+    public int GuideId { get; set; }
+}
---
+++ HwsBackend.Domain/Entities/Guide.cs
@@ -0,0 +1,13 @@
+namespace HwsBackend.Domain.Entities;
+public class Guide {
+    public int Id { get; set; }
+    public string Title { get; set; } 
+    public string Description { get; set; } 
+    public int DaysCount { get; set; } 
+    public MobilityType Mobility { get; set; } 
+    public Season Season { get; set; } 
+    public TargetAudience Target { get; set; } 
+    
+    public List<Activity> Activities { get; set; } = new(); 
+    public List<string> InvitedUserIds { get; set; } = new();
+}
